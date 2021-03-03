708f9a67828ca67924391a7524c01ed4b4eec92c
pike-msonda <pike.msonda@gmail.com>
2021-03-03 18:51:42 +0300

    added covid notebook

--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ def to_datetime(dtstring, dtformat):
     """
     """
     return datetime.datetime.strptime(dtstring, dtformat)
+
+def create_conditions(columns, value, condition,  operator='='):
+    condtions = ''
+    for i in range(len(columns)):
+       condtions += condtions+' '+columns[i] + operator + value +' '+condition
+    return condtions;
