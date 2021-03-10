6928e91ed6883ddaec4708956774c1eefbaa5ab9
Payk <pike.msonda@gmail.com>
2021-03-10 18:35:07 +0300

    addeed new graphs and labels

---
+++ utils.py
@@ -0,0 +1,89 @@
+def northern():
+    """Nothern African Countries"""
+    return [
+        'Algeria',
+        'Canary Islands',
+        'Ceuta',
+        'Egypt',
+        'Libya',
+        'Madeira',
+        'Melilla',
+        'Morocco',
+        'Sahrawai Araba Democratic Republic',
+        'Sudan',
+        'Sudan',
+        'Tunisia',
+    ]
+
+def eastern():
+    """Eastern African Countries"""
+    return [
+        'Burundi',
+        'Burundi',
+        'Comoros',
+        'Djibouti',
+        'Eritrea',
+        'Ethiopia',
+        'French Southern Territories',
+        'Kenya',
+        'Madagascar',
+        'Malawi',
+        'Mauritius',
+        'Mayotte',
+        'Mozambique',
+        'Réunion',
+        'Rwanda',
+        'Seychelles',
+        'Somalia',
+        'Somaliland',
+        'South Sudan',
+        'Tanzania',
+        'Uganda',
+        'Zambia',
+        'Zimbabwe'
+    ]
+
+
+def western():
+    """Western African Countries"""
+    return [
+        'Benin',
+        'Burkina Faso',
+        'Cape Verde',
+        'Ivory Coast',
+        'Gambia',
+        'Ghana',
+        'Guinea',
+        'Guinea-Bissau',
+        'Liberia',
+        'Mali',
+        'Mauritania',
+        'Niger',
+        'Nigeria'
+    ]
+
+def central():
+    """Central African Countries"""
+    return [
+        'Angola',
+        'Cameroon',
+        'Central African Republic',
+        'Chad',
+        'Democratic Republic of the Congo',
+        'Republic of the Congo',
+        'Equatorial Guinea',
+        'Gabon',
+        'São Tomé and Príncipe'
+    ]
+
+def southern():
+    """Southern African Countries"""
+    return [
+        'Botswana',
+        'Eswatini (Swaziland)',
+        'Lesotho',
+        'Namibia',
+        'South Africa'
+    ]
+
+
